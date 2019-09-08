3df3180ff39ceea5193be9b90aeb7982efe6b05b
sweetyclem <pirlot.clementine@gmail.com>
2019-09-08 10:47:23 -0400

    Only fetch clockins for one teacher

--- app/controllers/clock_ins_controller.rb
+++ app/controllers/clock_ins_controller.rb
@@ -5,7 +5,7 @@ class ClockInsController < ApplicationController
 
   # GET /clock_ins
   def index
-    @clock_ins = ClockIn.all.order(created_at: :desc)
+    @clock_ins = current_teacher.clock_ins.order(created_at: :desc)
     @current_teacher = current_teacher
   end
 
